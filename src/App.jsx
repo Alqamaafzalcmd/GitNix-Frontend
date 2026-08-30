f6952707f1e37a590854b176b6059655f31c7fb9
Alqama <alquamaafzal7462@gmail.com>
2026-08-30 23:35:47 +0530

    Created folder structure and routes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Welocome to GitNix</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Routes.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react'
+import {useNavigate, useRoutes} from 'react-router-dom'
+
+
+// pages list 
+import Dashboard from "./components/dashboard/Dashboard"
+import Profile from "./components/user/Profile"
+import Login from "./components/auth/Login"
+import Signup from "./components/auth/Signup"
+
+
+
+import {useAuth} from "./authContext";
+
+const ProjectRoutes = () => {
+    const [currUser, setCurrentUser] = useAuth();
+    const navigate = useNavigate();
+
+
+    useEffect (() =>{
+        const userIdTemp = localStorage.getItem('userId');
+
+        if(userIdTemp && !currUser){
+            setCurrentUser(userIdTemp);
+        }
+
+        if(!userIdTemp && !["/auth", "/signup"].includes(window.location.pathname)){
+            navigate("/auth");
+        }
+
+        if(userIdTemp && window.location.pathname === "/auth"){
+            navigate("/");
+        }
+    },[currUser, navigate, setCurrentUser]);
+
+
+    let element = useRoutes([
+      { path: "/", element: <Dashboard /> },
+      { path: "/auth", element: <Login /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "pfofile", element: <Profile /> },
+    ]);
+
+    return element;
+}
+
+
+export default ProjectRoutes;
